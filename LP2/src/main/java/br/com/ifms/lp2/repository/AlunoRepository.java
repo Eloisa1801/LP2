dc85da9dc4952462d327ba0512eb26eaea448bd5
Eloisa1801 <99847182+Eloisa1801@users.noreply.github.com>
2023-03-03 22:28:04 -0400

    Delete LP2 directory

--- LP2/src/main/java/br/com/ifms/lp2/repository/AlunoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.ifms.lp2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.ifms.lp2.javabeans.Aluno;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long>{
-}
